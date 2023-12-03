0cbe68e080b48fcd098514f9348218045e5ac39f
Ural Ataullin <ataullinural@gmail.com>
2023-12-03 20:40:58 +0500

    start models

--- starmen/views.py
+++ starmen/views.py
@@ -4,11 +4,31 @@ from django.shortcuts import render
 
 menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
 
+data_db = [
+    {'id': 1,
+     'title': 'Анджелина Джоли',
+     'content': 'Биография Анджелины Джоли',
+     'is_published': True},
+
+    {'id': 2,
+     'title': 'Марго Робби',
+     'content': 'Биография Марго Робби',
+     'is_published': False},
+
+    {'id': 3,
+     'title': 'Джулия Робертс',
+     'content': 'Биография Джулии Робертс',
+     'is_published': True},
+]
+
 
 def index(request):
     return render(
-        request, "starmen/index.html", {"menu": menu,
-                                        "title": "Главная страница"}
+        request,
+        "starmen/index.html",
+        {"menu": menu,
+         "title": "Главная страница",
+         "post": data_db},
     )
 
 
