4fcfd7782c43db2bc9eab56123937a699e8f2670
Ural Ataullin <ataullinural@gmail.com>
2023-12-08 17:39:32 +0500

    create and migrate models

--- starmen/models.py
+++ starmen/models.py
@@ -1,13 +1,9 @@
-# from django.db import models
+from django.db import models
 
 
-# class Starmen(models.Model):
-#     title = models.CharField(max_length=255)
-#     content = models.TextField(blank=True)
-#     photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
-#     time_create = models.DateTimeField(auto_now_add=True)
-#     time_update = models.DateTimeField(auto_now=True)
-#     is_published = models.BooleanField(default=True)
-
-#     def __str__(self):
-#         return self.title
+class Starmen(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=True)
