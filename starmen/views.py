d36be98124d4fc848cb57d550c8b9ac7c4303d3c
Ural Ataullin <ataullinural@gmail.com>
2024-01-11 20:12:59 +0500

    Starmen.Status.PUBLISHED

--- starmen/views.py
+++ starmen/views.py
@@ -10,15 +10,6 @@ menu = [
     {'title': 'Обратная связь', 'url_name': 'contact'},
     {'title': 'Войти', 'url_name': 'login'}]
 
-data_db = [
-    {'id': 1, 'title': 'Альберт Эйнштейн', 'content': '''<h1>Альберт Эйнштейн</h1> американский, немецкий и швейцарский физик-теоретик и общественный деятель-гуманист, один из основателей современной теоретической физики. Лауреат Нобелевской премии по физике 1921 года. Его теория относительности изменила основания физики, заменив классическую механику и закон всемирного тяготения Ньютона.''',
-     'is_published': True},
-    {'id': 2, 'title': 'Генри Форд',
-        'content': 'Биография Генри Форд', 'is_published': False},
-    {'id': 3, 'title': 'Махатма Ганди',
-        'content': 'Биография Махатма Ганди', 'is_published': True},
-]
-
 cats_db = [
     {'id': 1, 'name': 'Ученые'},
     {'id': 2, 'name': 'Предприниматели'},
@@ -27,7 +18,7 @@ cats_db = [
 
 
 def index(request):
-    posts = Starmen.objects.filter(is_published=1)
+    posts = Starmen.published.all()
     data = {
         'title': 'Главная страница',
         'menu': menu,
@@ -76,7 +67,7 @@ def show_category(request, cat_id):
     data = {
         'title': 'Отображение по рубрикам',
         'menu': menu,
-        'posts': data_db,
+        'posts': Starmen.published.all(),
         'cat_selected': cat_id
     }
     return render(request, 'starmen/index.html', data)
