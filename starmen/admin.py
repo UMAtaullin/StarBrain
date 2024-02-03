44ad483d7534232363c780416867b49ddee17ab8
Ural Ataullin <ataullinural@gmail.com>
2024-02-03 16:38:04 +0500

    setting up the display for admin

--- starmen/admin.py
+++ starmen/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
-from .models import Starmen
+from .models import Starmen, Category
 
 
-admin.site.register(Starmen)
+@admin.register(Starmen)  # admin.site.register(Starmen, StarmenAdmin)
+class StarmenAdmin(admin.ModelAdmin):
+    list_display = ('id',
+                    'title',
+                    'time_create',
+                    'is_published',
+                    'cat')
+    list_display_links = ('id',
+                          'title')
+    ordering = ['time_create', 'title']
+    list_editable = ('is_published', 'cat')
+    list_per_page = 5
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id',
+                    'name')
+    list_display_links = ('id',
+                          'name')
--- starmen/models.py
+++ starmen/models.py
@@ -15,26 +15,37 @@ class Starmen(models.Model):
         DRAFT = 0, 'Еще редактируется'
         PUBLISHED = 1, 'Опубликовано'
 
-    title = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=255, unique=True, db_index=True)
-    content = models.TextField(blank=True)
-    time_create = models.DateTimeField(auto_now_add=True)
-    time_update = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=255,
+                             verbose_name='Заголовок')
+    slug = models.SlugField(max_length=255,
+                            unique=True,
+                            db_index=True)
+    content = models.TextField(blank=True,
+                               verbose_name='Текст статьи')
+    time_create = models.DateTimeField(auto_now_add=True,
+                                       verbose_name='Время создания')
+    time_update = models.DateTimeField(auto_now=True,
+                                       verbose_name='Время изменения')
     is_published = models.BooleanField(
-        choices=Status.choices, default=Status.PUBLISHED)
-    cat = models.ForeignKey('Category', on_delete=models.PROTECT,
-                            related_name='posts')
-    tags = models.ManyToManyField('TagPost', blank=True, related_name='tags')
+        choices=tuple(map(lambda x: (bool(x[0]), x[1]), Status.choices)),
+        default=Status.DRAFT,
+        verbose_name="Статус")
+    cat = models.ForeignKey('Category',
+                            on_delete=models.PROTECT,
+                            related_name='posts',
+                            verbose_name='Категории')
+    tags = models.ManyToManyField('TagPost',
+                                  blank=True,
+                                  related_name='tags',
+                                  verbose_name='Теги')
     spouse = models.OneToOneField('Spouse',
                                   on_delete=models.SET_NULL,
                                   null=True, blank=True,
-                                  related_name='partner')
+                                  related_name='partner',
+                                  verbose_name='Супруги')
 
     published = PublishedManager()
     objects = models.Manager()
-
-    def __str__(self) -> str:
-        return self.title
 
     class Meta:
         verbose_name = 'Известные люди'
@@ -42,16 +53,25 @@ class Starmen(models.Model):
         ordering = ['-time_create']
         indexes = [models.Index(fields=['time_create'])]
 
+    def __str__(self) -> str:
+        return self.title
+
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_slug': self.slug})
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=64, db_index=True)
+    name = models.CharField(max_length=64,
+                            db_index=True,
+                            verbose_name='Название категории')
     slug = models.SlugField(max_length=255, unique=True, db_index=True)
 
     def get_absolute_url(self):
         return reverse('category', kwargs={'cat_slug': self.slug})
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
 
     def __str__(self):
         return self.name
